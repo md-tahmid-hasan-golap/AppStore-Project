b6b72c030e9a34ddcafd623973d69431740c3ee6
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-07-13 23:39:25 +0600

    add a banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,57 +2,56 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="py-5 flex justify-center">
-      {" "}
-      <div className="carousel rounded-box w-64">
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
+    <div className="carousel w-full rounded-xl shadow-xl py-8">
+      {/* Slide 1 */}
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/1YD77d8Z/Screenshot-2025-07-13-232718.png"
+          className="w-full object-cover h-[300px]"
+          alt="Promotion 1"
+        />
+        <div className="absolute flex items-center justify-between transform -translate-y-1/2 left-4 right-4 top-1/2">
+          <a href="#slide3" className="btn btn-circle bg-gray-800 text-white">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle bg-gray-800 text-white">
+            ❯
+          </a>
         </div>
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
+        <div className="absolute bottom-5 left-5 text-white bg-black/50 p-3 rounded"></div>
+      </div>
+
+      {/* Slide 2 */}
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/wZGnscCy/Screenshot-2025-07-13-233041.png"
+          className="w-full object-cover h-[300px]"
+          alt="Promotion 2"
+        />
+        <div className="absolute flex items-center justify-between transform -translate-y-1/2 left-4 right-4 top-1/2">
+          <a href="#slide1" className="btn btn-circle bg-gray-800 text-white">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle bg-gray-800 text-white">
+            ❯
+          </a>
         </div>
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
-        </div>
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
-        </div>
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
-        </div>
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
-        </div>
-        <div className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-            className="w-full"
-            alt="Tailwind CSS Carousel component"
-          />
+      </div>
+
+      {/* Slide 3 */}
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/SwjJsZDg/Screenshot-2025-07-13-233237.png"
+          className="w-full object-cover h-[300px]"
+          alt="Promotion 3"
+        />
+        <div className="absolute flex items-center justify-between transform -translate-y-1/2 left-4 right-4 top-1/2">
+          <a href="#slide2" className="btn btn-circle bg-gray-800 text-white">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle bg-gray-800 text-white">
+            ❯
+          </a>
         </div>
       </div>
     </div>
